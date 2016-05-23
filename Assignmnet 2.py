e62bab37854696790ba3a0bcfd294d3941c638fc
JAmesMoggJCU <james.mogg@my.jcu.edu.au>
2016-05-23 13:55:17 +1000

    Assignment 2
    Completed the menu options of ui

--- Assignmnet 2.py
+++ Assignmnet 2.py
@@ -1,35 +1,48 @@
 from kivy.app import App
 from kivy.lang import Builder
 
-class ItemsForHire(App):
+class ItemsForHireApp(App):
+
     def __init__(self, **kwargs):
-        super(ItemsForHire, self). __init__(**kwargs)
+
+        super(ItemsForHireApp, self).__init__(**kwargs)
+        self.items_list = []
+        in_file = open("items.csv", "r")
+        for line in in_file:
+            item_match = line.strip().replace(" ", "").split(",")
+            name = item_match[0]
+            description = item_match[1]
+            price = item_match[2]
+
+            self.items_list.append((item_match[0], item_match[1], item_match[2]))
 
     def build(self):
-        self.title = 'Items For Hire'
+        # Window.size = (350, 700)
+        self.title = "Items For Hire"
         self.root = Builder.load_file('Assignment.kv')
-        self.create_menu_buttons()
         return self.root
 
-    def Hire_button (self):
+    def press_add(self):
+        self.root.ids.popup.open()
 
-    #def List_Item_Button (self):
-     newlist = []
-     in_file = open("items.csv", "r")
-     #splits the data from the csv and creates it into a tuple
-     for line in in_file:
-      item = line.strip().replace("","").split(",")
-      name = item[0]
-      description = item[1]
-      price = item[2]
-      hired = item[3]
-      newlist.append((item[0], item[1], item[2], item[3]))
+    def press_cancel(self):
+        self.root.ids.popup.dismiss()
 
-      in_file.close()
+    def press_save(self):
+        self.root.ids.popup.dismiss()
 
 
-    #def Add_New_Item_Button (self):
+    def show_all_items(self):
+        # this is the function that prints all items in the list
+        print("All items on file (* indicates item is currently out):")
+        item_count_number = -1
+        for printing_items in self.items_list:
+            if len(printing_items[3]) == 3:
+                item_count_number += 1
+                print("{0} = {1} = ${2} *".format(item_count_number, printing_items[0:2], printing_items[2]))
+            else:
+                item_count_number += 1
+                print("{0} = {1} = ${2}".format(item_count_number, printing_items[0:2], printing_items[2]))
 
-    #def Return_Item_Button (self):
 
-    #def Continue_Button (self):
+ItemsForHireApp().run()
